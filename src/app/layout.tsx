69563b3a9e84f25e14d2b54ac62466d5eee3e986
cauefer <cauefernandescaetano@gmail.com>
2024-12-07 21:15:07 -0300

    FEAT: navbar & footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 import { Inter } from "next/font/google";
+import { ThemeProvider } from "@material-tailwind/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
-import Image from "next/image";
+import { DefaultTopNav } from "@/components/nav/DefaultTopNav";
+import DefaultFooter from "@/components/ui/footer/DefaultFooter";
 
 export default function Home() {
-  return <div className="items-center justify-center min-h-screen"></div>;
+  return (
+    <div className="items-center justify-center min-h-screen">
+      <DefaultTopNav />
+
+      <main className="flex flex-1 "></main>
+
+      <DefaultFooter />
+    </div>
+  );
 }
